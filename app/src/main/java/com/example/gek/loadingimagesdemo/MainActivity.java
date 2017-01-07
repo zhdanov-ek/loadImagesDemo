faa3ea887e698423a2edd2bcdece52ef91308099
Zhdanov Evgeniy <zhdanov.ek@gmail.com>
2017-01-07 22:40:43 +0200

    Show loading images with Glide and Picasso. Picasso not use caching.

--- app/src/main/java/com/example/gek/loadingimagesdemo/MainActivity.java
+++ app/src/main/java/com/example/gek/loadingimagesdemo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.gek.loadingimagesdemo;
 
 import android.content.Context;
+import android.net.Uri;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,12 +9,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
+
+/**
+ * Сравнение глайд и пикассо
+ * https://inthecheesefactory.com/blog/get-to-know-glide-recommended-by-google/en
+ * <p>
+ * Демонстрация работы с библиотеками
+ */
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnGlide, btnPicasso;
-    ImageView iv1, iv2, iv3, iv4, iv5;
+    Button btnLoad;
+    ImageView ivG0, ivG1, ivG2, ivG3, ivG4;
+    ImageView ivP0, ivP1, ivP2, ivP3, ivP4;
     ArrayList<String> links;
     Context ctx;
 
@@ -25,31 +37,56 @@ public class MainActivity extends AppCompatActivity {
         links = Photos.getLinks();
         ctx = this;
 
-        btnGlide = (Button) findViewById(R.id.btnGlide);
-        btnGlide.setOnClickListener(clickListener);
-        btnPicasso = (Button) findViewById(R.id.btnPicasso);
-        btnPicasso.setOnClickListener(clickListener);
+        btnLoad = (Button) findViewById(R.id.btnLoad);
+        btnLoad.setOnClickListener(clickListener);
 
-        iv1 = (ImageView) findViewById(R.id.iv1);
-        iv2 = (ImageView) findViewById(R.id.iv2);
-        iv3 = (ImageView) findViewById(R.id.iv3);
-        iv4 = (ImageView) findViewById(R.id.iv4);
-        iv5 = (ImageView) findViewById(R.id.iv5);
+        ivG0 = (ImageView) findViewById(R.id.ivG0);
+        ivG1 = (ImageView) findViewById(R.id.ivG1);
+        ivG2 = (ImageView) findViewById(R.id.ivG2);
+        ivG3 = (ImageView) findViewById(R.id.ivG3);
+        ivG4 = (ImageView) findViewById(R.id.ivG4);
+
+        ivP0 = (ImageView) findViewById(R.id.ivP0);
+        ivP1 = (ImageView) findViewById(R.id.ivP1);
+        ivP2 = (ImageView) findViewById(R.id.ivP2);
+        ivP3 = (ImageView) findViewById(R.id.ivP3);
+        ivP4 = (ImageView) findViewById(R.id.ivP4);
     }
 
     View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            switch (view.getId()){
-                case R.id.btnGlide:
+            Glide.with(ctx)
+                    .load(R.drawable.person_default)
+                    .into(ivG0);
+            Glide.with(ctx)
+                    .load(links.get(1))
+                    .into(ivG1);
+            Glide.with(ctx)
+                    .load(links.get(2))
+                    .into(ivG2);
+            Glide.with(ctx)
+                    .load(links.get(3))
+                    .into(ivG3);
+            Glide.with(ctx)
+                    .load(links.get(4))
+                    .into(ivG4);
 
-                    break;
-
-                case R.id.btnPicasso:
-
-                    break;
-
-            }
+            Picasso.with(ctx)
+                    .load(R.drawable.person_default)
+                    .into(ivP0);
+            Picasso.with(ctx)
+                    .load(Uri.parse(links.get(1)))
+                    .into(ivP1);
+            Picasso.with(ctx)
+                    .load(Uri.parse(links.get(2)))
+                    .into(ivP2);
+            Picasso.with(ctx)
+                    .load(Uri.parse(links.get(3)))
+                    .into(ivP3);
+            Picasso.with(ctx)
+                    .load(Uri.parse(links.get(4)))
+                    .into(ivP4);
 
         }
     };
